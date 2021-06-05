5593c5f54c9fc17d22ab27a82967694d67a1d037
Peter Taylor <peter@peterhenryemerson.co.uk>
2021-06-05 09:26:04 +0100

    mili is thousandth, not hundreth

--- Program.cs
+++ Program.cs
@@ -13,7 +13,7 @@ namespace MembershipScraperNG
             while(true){
                 Console.WriteLine("Starting Task");
                 var x = s.GetMembers();
-                await Task.Delay((int)(100*60*2.5));
+                await Task.Delay((int)(1000*60*2.5));
                 await x;
             }
         }
